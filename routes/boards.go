708b03fc8f30791064830a860a7ab157d1c1afa5
triskil <trisskil@gmail.com>
2022-12-15 17:02:16 -0500

    Add todo data models.

--- routes/boards.go
+++ routes/boards.go
@@ -2,12 +2,17 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
+	"time"
 )
 
 type Board struct {
-	Uuid string
-	Name string
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	Name       string             `bson:"name,omitempty"`
+	CreatedTS  time.Time          `bson:"created_ts,omitempty"`
+	ModifiedTS time.Time          `bson:"modified_ts,omitempty"`
+	OwnerID    primitive.ObjectID `bson:"owner_id,omitempty"`
 }
 
 func Boards(ctx echo.Context) error {
--- routes/lists.go
+++ routes/lists.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type BoardList struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	Name       string             `bson:"name,omitempty"`
+	Order      int32              `bson:"order,omitempty"`
+	CreatedTS  time.Time          `bson:"created_ts,omitempty"`
+	ModifiedTS time.Time          `bson:"modified_ts,omitempty"`
+	BoardID    primitive.ObjectID `bson:"board_id,omitempty"`
+}
