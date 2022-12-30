24edc4ca1c0635e48b3df960786e2fb7972007d0
triskil <trisskil@gmail.com>
2022-12-30 11:02:25 -0500

    Move structs to models package.

--- controller/tasks_controller.go
+++ controller/tasks_controller.go
@@ -1,16 +1,2 @@
 package controller
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"time"
-)
-
-type Task struct {
-	ID         primitive.ObjectID `bson:"_id,omitempty"`
-	Name       string             `bson:"name,omitempty"`
-	Order      int32              `bson:"order,omitempty"`
-	Content    string             `bson:"content,omitempty"`
-	CreatedTS  time.Time          `bson:"created_ts,omitempty"`
-	ModifiedTS time.Time          `bson:"modified_ts,omitempty"`
-	ListID     primitive.ObjectID `bson:"list_id,omitempty"`
-}
